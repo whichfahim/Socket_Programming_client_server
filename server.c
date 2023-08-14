c7e0bf442120b56a8821dd8094e8acc0f9e36e40
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-08-14 13:55:26 -0500

    Fahim | filesrch | working filesrch code

--- server.c
+++ server.c
@@ -3,242 +3,14 @@
 #include <stdlib.h>
 #include <sys/socket.h> //for socket APIs
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <time.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <fcntl.h>
-
-/*
-    CLIENT COMMANDS
-    ===============
-*/
-void fgets_command(int client_sd, char buff1[])
-{
-    const char *file_list = buff1 + 6; // Extract the list of files from the command
-    printf("List of files: %s\n", file_list);
-
-    // Tokenize the file list by space
-    char *file_token = strtok(file_list, " ");
-    int file_count = 0;
-    char file_list_str[1024] = ""; // Buffer to store the file list
-
-    // Takes up to 4 files as parameters
-    while (file_token != NULL && file_count <= 4)
-    {
-        // Check if the file exists
-        if (access(file_token, F_OK) != -1)
-        {
-            // Concatenate file_list_str with file_token
-            strcat(file_list_str, file_token);
-            // Insert space in between
-            strcat(file_list_str, " ");
-
-            // only increment count if file exists
-            file_count++;
-        }
-        else
-        {
-            // printf("File not found: %s\n", file_token);
-            char msg[100];
-            sprintf(msg, "File not found: %s\n", file_token);
-            // send(client_sd, msg, strlen(not_found_msg), 0);
-
-            write(client_sd, msg, 100);
-        }
-
-        file_token = strtok(NULL, " ");
-    }
-
-    if (file_count > 0)
-    {
-        printf("Files found: %d\n", file_count);
-
-        // Create a tar archive of all the files
-        char tar_command[1024];
-        snprintf(tar_command, sizeof(tar_command), "tar -czf temp.tar.gz %s", file_list_str);
-
-        system(tar_command);
-
-        // Send the tar archive to the client
-        FILE *tar_file = fopen("temp.tar.gz", "rb");
-
-        if (tar_file)
-        {
-            fseek(tar_file, 0, SEEK_END);
-            long tar_size = ftell(tar_file);
-            rewind(tar_file);
-
-            char *tar_buffer = (char *)malloc(tar_size);
-            fread(tar_buffer, 1, tar_size, tar_file);
-            fclose(tar_file);
-
-            send(client_sd, tar_buffer, tar_size, 0);
-
-            free(tar_buffer);
-        }
-        else
-        {
-            const char *error_msg = "Error creating tar.gz file";
-            send(client_sd, error_msg, strlen(error_msg), 0);
-        }
-    }
-    else
-    {
-        // const char *not_found_msg = "No file found";
-        char *msg = "No files found";
-
-        write(client_sd, msg, 50);
-    }
-}
-
-// =======TARFGETZ SIZE1 SIZE2===========
-void tarfgetz(int client_sd, char buff1[])
-{
-
-    // code here
-    unsigned long long minSize, maxSize;
-    // returns the number of fields that were successfully converted and assigned
-
-    int assigned = sscanf(buff1 + 9, "%llu %llu", &minSize, &maxSize);
-
-    // validation for client input
-    if (assigned >= 2 && minSize <= maxSize)
-    {
-        // only perform these operations if command format is accurate
-        sscanf(buff1 + 9, "%llu %llu", &minSize, &maxSize);
-        printf("Requested size range: %llu - %llu\n", minSize, maxSize);
-
-        // unsigned long long minSize = 0; // Minimum file size in bytes
-        // unsigned long long maxSize = 1000; // Maximum file size in bytes
-
-        char *fileList = (char *)malloc(1); // Start with an empty string
-        if (fileList == NULL)
-        {
-            perror("malloc");
-            return EXIT_FAILURE;
-        }
-        fileList[0] = '\0';
-
-        findFilesInSizeRange(minSize, maxSize, &fileList);
-
-        // Create a temporary file to store the list of files
-        char tmpFilePath[] = "/tmp/file_list.txt";
-        int tmpFile = open(tmpFilePath, O_CREAT | O_WRONLY, 0644);
-        if (tmpFile == -1)
-        {
-            perror("open");
-            free(fileList);
-            return EXIT_FAILURE;
-        }
-        write(tmpFile, fileList, strlen(fileList));
-        close(tmpFile);
-
-        // Create tar.gz archive
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            chdir("."); // Change to the current directory
-            execlp("tar", "tar", "-czvf", "temp.tar.gz", "-T", tmpFilePath, NULL);
-            perror("execlp");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid > 0)
-        {
-            wait(NULL);
-        }
-        else
-        {
-            perror("fork");
-            free(fileList);
-            return EXIT_FAILURE;
-        }
-
-        printf("Tar.gz archive created: temp.tar.gz\n");
-
-        // Delete the temporary file
-        if (unlink(tmpFilePath) != 0)
-        {
-            perror("unlink");
-        }
-
-        free(fileList);
-    }
-    else
-    {
-        char *msg = "Usage: tarfgetz minSize maxSize";
-
-        write(client_sd, msg, 50);
-        // printf("Wrong input format.\n");
-        // printf("Usage: tarfgetz minSize maxSize\n");
-        // printf("Where, minSize<maxSize");
-    }
-}
-
-void filesrch(int client_sd, char buff1[])
-{
-    const char *filename = buff1 + 9; // Extract the filename from the command
-    printf("Requested file: %s\n", filename);
-
-    // Get the user's home directory
-    const char *home_dir = getenv("HOME");
-    printf("Searching directory tree rooted at: %s\n", home_dir);
-
-    // Recursively search the home directory for the file
-    searchDirectory(client_sd, home_dir, filename);
-
-    // File not found
-    char *msg = "File not found";
-    write(client_sd, msg, 100);
-}
-
-void processClient(int client_sd)
-{
-    // printf("Message from the client\n");
-    char buff1[1024];
-    ssize_t bytes_read = read(client_sd, buff1, sizeof(buff1) - 1);
-
-    if (bytes_read <= 0)
-    {
-        perror("read");
-        close(client_sd);
-        return;
-    }
-
-    buff1[bytes_read] = '\0';
-    // printf("Client command: %s\n", buff1);
-
-    if (strcmp(buff1, "quit") == 0)
-    {
-        printf("Quitting...");
-        // break;
-    }
-    // compare the first 6 characters with "fgets"
-    else if (strncmp(buff1, "fgets ", 6) == 0)
-    {
-        // execute fgets command
-        fgets_command(client_sd, buff1);
-    }
-    // compare first 8 characters of buff1 with "tarfgetz"
-    else if (strncmp(buff1, "tarfgetz ", 8) == 0)
-    {
-        // execute tarfgetz command
-        tarfgetz(client_sd, buff1);
-    }
-    else if (strncmp(buff1, "filesrch ", 8) == 0)
-    {
-        // code here
-        filesrch(client_sd, buff1);
-    }
-    else if (strncmp(buff1, "targzf ", 6) == 0)
-    {
-        // code here
-    }
-    else if (strncmp(buff1, "getdirf ", 7) == 0)
-    {
-        // code here
-    }
-}
+#include <unistd.h>
+#include <arpa/inet.h>
 
 // ==== UTILITY METHODS =====
 void searchDirectory(int client_sd, const char *search_path, const char *filename)
@@ -282,54 +54,74 @@ void searchDirectory(int client_sd, const char *search_path, const char *filenam
     closedir(dir);
 }
 
-void findFilesInSizeRange(unsigned long long minSize, unsigned long long maxSize, char **fileList)
+/*
+    CLIENT COMMANDS
+    ===============
+*/
+
+void filesrch(int client_sd, char buff1[])
 {
-    DIR *dir = opendir(".");
-    if (dir == NULL)
+    const char *filename = buff1 + 9; // Extract the filename from the command
+    printf("Requested file: %s\n", filename);
+
+    // Get the user's home directory
+    const char *home_dir = getenv("HOME");
+    printf("Searching directory tree rooted at: %s\n", home_dir);
+
+    // Recursively search the home directory for the file
+    searchDirectory(client_sd, home_dir, filename);
+
+    // File not found
+    char *msg = "File not found";
+    write(client_sd, msg, 100);
+}
+
+void processClient(int client_sd)
+{
+    // printf("Message from the client\n");
+    char buff1[1024];
+    ssize_t bytes_read = read(client_sd, buff1, sizeof(buff1) - 1);
+
+    if (bytes_read <= 0)
     {
-        perror("opendir");
+        perror("read");
+        close(client_sd);
         return;
     }
 
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL)
+    buff1[bytes_read] = '\0';
+    // printf("Client command: %s\n", buff1);
+
+    if (strcmp(buff1, "quit") == 0)
     {
-        if (entry->d_type == DT_REG)
-        { // Regular file
-            char filePath[256];
-            snprintf(filePath, sizeof(filePath), "./%s", entry->d_name);
-
-            struct stat fileStat;
-            if (stat(filePath, &fileStat) == 0)
-            {
-                if (S_ISREG(fileStat.st_mode) && fileStat.st_size >= minSize && fileStat.st_size <= maxSize)
-                {
-                    appendFilePath(fileList, entry->d_name);
-                }
-            }
-            else
-            {
-                perror("stat");
-            }
-        }
+        printf("Quitting...");
+        // break;
     }
-
-    closedir(dir);
-}
-
-void appendFilePath(char **fileList, const char *filePath)
-{
-    size_t currentSize = strlen(*fileList);
-    size_t filePathSize = strlen(filePath);
-    char *newList = realloc(*fileList, currentSize + filePathSize + 2); // +2 for newline and null terminator
-    if (newList == NULL)
+    // compare the first 6 characters with "fgets"
+    else if (strncmp(buff1, "fgets ", 6) == 0)
     {
-        perror("realloc");
-        exit(EXIT_FAILURE);
+        // execute fgets command
+        // fgets_command(client_sd, buff1);
     }
-    *fileList = newList;
-    strcat(*fileList, filePath);
-    strcat(*fileList, "\n");
+    // compare first 8 characters of buff1 with "tarfgetz"
+    else if (strncmp(buff1, "tarfgetz ", 8) == 0)
+    {
+        // execute tarfgetz command
+        // tarfgetz(client_sd, buff1);
+    }
+    else if (strncmp(buff1, "filesrch ", 8) == 0)
+    {
+        // code here
+        filesrch(client_sd, buff1);
+    }
+    else if (strncmp(buff1, "targzf ", 6) == 0)
+    {
+        // code here
+    }
+    else if (strncmp(buff1, "getdirf ", 7) == 0)
+    {
+        // code here
+    }
 }
 
 int main(int argc, char *argv[])
